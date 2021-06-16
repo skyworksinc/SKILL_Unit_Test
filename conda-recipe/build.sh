fa80ba0e9f784380eb0cbfe00ac1c5b8ea57eaa6
tiffmay <tiff.mayberry@gmail.com>
2021-06-16 09:21:22 -0500

    #3 update build.sh (add src dir to path)

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,8 +1,8 @@
 # Install Virtuoso skill interface 
-mkdir -p "${PREFIX}/lib/skill/SkillUnitTest"
+mkdir -p "${PREFIX}/lib/skill/SkillUnitTest/src"
 
 cp -f "${RECIPE_DIR}/../src/utTest.ils" \
-       "${PREFIX}/lib/skill/SkillUnitTest/utTest.ils"
+       "${PREFIX}/lib/skill/SkillUnitTest/src/utTest.ils"
 
 cp -f "${RECIPE_DIR}/../skill-unit-test.init.ils" \
        "${PREFIX}/lib/skill/SkillUnitTest/skill-unit-test.init.ils"
