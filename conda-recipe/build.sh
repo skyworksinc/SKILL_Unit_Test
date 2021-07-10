e10682fc3f91a491bd593578ac9400471319729c
Curtis Mayberry <Curtisma3@gmail.com>
2021-07-10 00:42:16 -0500

    Update build.sh
    Updated package name to `skill-unit-test`

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,8 +1,8 @@
-# Install Virtuoso skill interface 
-mkdir -p "${PREFIX}/lib/skill/SkillUnitTest/src"
+# Install Virtuoso skill package
+mkdir -p "${PREFIX}/lib/skill/skill-unit-test/src"
 
 cp -f "${RECIPE_DIR}/../src/utTest.ils" \
-       "${PREFIX}/lib/skill/SkillUnitTest/src/utTest.ils"
+       "${PREFIX}/lib/skill/skill-unit-test/src/utTest.ils"
 
 cp -f "${RECIPE_DIR}/../skill-unit-test.init.ils" \
-       "${PREFIX}/lib/skill/SkillUnitTest/skill-unit-test.init.ils"
+       "${PREFIX}/lib/skill/skill-unit-test/skill-unit-test.init.ils"
